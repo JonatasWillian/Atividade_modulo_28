fcceadb67d22537a54a9df5df181e1ae93e92d9e
JonatasWillian <91629214+JonatasWillian@users.noreply.github.com>
2022-09-20 10:01:32 -0300

    retirado o codigo comentado

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -21,6 +21,10 @@ public class Player : MonoBehaviour, IDamageable
     [Header("Flash")]
     public List<FlashColor> flashColors;
 
+    [Header("Boss")]
+    public GameObject boss;
+    public bool gameObjectBoss = false;
+
     #region LIFE
     public void Damage(float damage)
     {
@@ -71,83 +75,4 @@ public class Player : MonoBehaviour, IDamageable
 
         animator.SetBool("Run", isWalking);
     }
-
-    /*public Rigidbody myRigidbody;
-
-    public Animator animator;
-
-    [Header("Movement")]
-    public Vector3 directionZ;
-    public Vector3 directionX;
-
-    [Header("Jump")]
-    private Vector3 velocity;
-    public float forceJump = 2;
-
-    private string animationName;
-
-    private void Awake()
-    {
-        animationName = "Idle";
-    }
-
-    private void Update()
-    {
-        Movement();
-        Jump();
-    }
-
-    private void Movement()
-    {
-        bool moving = false;
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            moving = true;
-            transform.Translate(directionZ * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            moving = true;
-            transform.Translate(-directionZ * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            moving = true;
-            transform.Translate(directionX * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            moving = true;
-            transform.Translate(-directionX * Time.deltaTime);
-        }
-
-        if (moving)
-        {
-            if (animationName != "Walking")
-            {
-                Debug.Log("Changing animation to walking");
-                animator.SetTrigger("Walking");
-                animationName = "Walking";
-            }
-        }
-        else
-        {
-            if (animationName != "Idle")
-            {
-                Debug.Log("Changing animation to Idle");
-                animator.SetTrigger("Idle");
-                animationName = "Idle";
-            }
-        }
-    }
-
-    private void Jump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            myRigidbody.velocity = Vector3.up * forceJump;
-        }
-    }*/
 }
